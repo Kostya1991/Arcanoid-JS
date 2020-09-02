6655836ad461fec12d3693a5ce1acabebcdf5fff
Lysov.Konstantin <Kosta-Lysov@mail.ru>
2020-09-02 13:35:24 +0300

    добавил favicon

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,7 @@ import background from '../assets/img/background.png';
 import ball from '../assets/img/ball.png';
 import block from '../assets/img/block.png';
 import platform from '../assets/img/platform.png';
+
 import bump from '../assets/sounds/bump.mp3';
 
 //********************** переменные ************************/
