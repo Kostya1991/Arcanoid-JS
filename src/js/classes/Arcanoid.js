e97264ecdb55b867d070b4eec0a7729fb105fbd1
Lysov.Konstantin <Kosta-Lysov@mail.ru>
2020-09-02 11:24:57 +0300

    добавлен прелоадер

--- src/js/classes/Arcanoid.js
+++ src/js/classes/Arcanoid.js
@@ -1,4 +1,5 @@
 import Events from './Events';
+import Preload from './Preload';
 
 export default class Arcanoid {
   constructor(ctx, running, score, blocks, sprites, sounds) {
@@ -19,9 +20,11 @@ export default class Arcanoid {
 
   start() {
     this.init();
-    // this.preload(() => {
-    //   this.create();
-    //   this.run();
-    // });
+    Preload.preload(this.sprites, this.sounds, () => {
+      this.sprites = Preload.getSprites();
+      this.sounds = Preload.getSounds();
+      // this.create();
+      // this.run();
+    });
   }
 }
